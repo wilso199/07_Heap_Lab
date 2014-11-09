6d4b5234f072ca8fc9198d0b4665caa2adeb2d86
Brandon Wilson <wilso199@miamioh.edu>
2014-11-09 11:39:20 -0500

    Removed Heap.ipp and completed constructor, destructor, grow() and getNumItems() methods

--- Heap.h
+++ Heap.h
@@ -46,4 +46,61 @@ private:
   void trickleDown(unsigned long index);  
 };
 
-#include "Heap.ipp"
+#include <string>
+
+template<class Pri, class T>
+Heap<Pri,T>::Heap(){
+    backingArray = new std::pair<Pri, T>[START_SIZE];
+    arrSize = START_SIZE;
+    numItems = 0;
+}
+
+template<class Pri, class T>
+Heap<Pri,T>::~Heap(){
+    delete[] backingArray;
+}
+
+template<class Pri, class T>
+void Heap<Pri,T>::grow(){
+    
+    int doubledSize = arrSize *2;
+    
+    std::pair<Pri, T>* newArray = new std::pair<Pri, T>[doubledSize];
+    
+    for(int i = 0; i < numItems; i++){
+        newArray[i] = backingArray[i];
+    }
+    
+    delete[] backingArray;
+    backingArray = newArray;
+    arrSize = doubledSize;
+}
+
+template<class Pri, class T>
+void Heap<Pri,T>::add(std::pair<Pri,T> toAdd){
+    //TODO
+}
+
+template<class Pri, class T>
+void Heap<Pri,T>::bubbleUp(unsigned long index){
+    //TODO
+}
+
+template<class Pri, class T>
+void Heap<Pri,T>::trickleDown(unsigned long index){
+    //TODO
+}
+
+template<class Pri, class T>
+std::pair<Pri,T> Heap<Pri,T>::remove(){
+    //TODO
+    std::pair<Pri,T> tmp;
+    return tmp;
+}
+
+template<class Pri, class T>
+unsigned long Heap<Pri,T>::getNumItems(){
+    
+    return numItems;
+}
+
